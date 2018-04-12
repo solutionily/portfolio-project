1b6525651a3003600c89430668e41b0195a5e337
Frederick Musser <solutionily@gmail.com>
2018-04-12 09:53:08 -0400

    Added job funtions to front page

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
 class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
     image = models.ImageField(upload_to='images/')
-    summery = models.CharField(max_length=200)# Create your models here.
+    # Create your models here.
